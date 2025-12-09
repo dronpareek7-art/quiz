39ccb574898751148444044e9b4b77a045f69545
Dron Pareek <dronpareek@example.com>
2025-12-09 15:01:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+timerdiv = document.querySelector(".timer");
+optionsdiv = document.querySelectorAll(".options p");
+questionsdiv = document.querySelector(".question");
+quiz = document.querySelector(".quiz");
+
+const data = [
+  {
+    q: "What is the capital of india",
+    a: "New delhi",
+    option: ["Mumbai", "churu", "New delhi ", "kolkata"],
+  },
+  {
+    q: "Which planet is the closet to the sun",
+    a: "Mercury",
+    option: ["Earth", "Mars", "Mercury ", "Venus"],
+  },
+  {
+    q: "Which is the largest ocean in the world",
+    a: "Pacific Ocean",
+    option: ["Pacific Ocean", "Indian Ocean", "Arctic Ocean", "Churu"],
+  },
+  {
+    q: "Where is Murtikala ",
+    a: "near FSL",
+    option: ["near FSL", "Gopalpura", "Churu", "bundi"],
+  },
+  {
+    q: "where is FSL",
+    a: "Murtikala",
+    option: ["gopalpura", "churu", "Murtikala ", "jaipur"],
+  },
+];
+
+let questionIndex = 0;
+let count = 5;
+timerdiv.innerText = count;
+
+function printquestionsandoption() {
+  questionsdiv.innerText = data[questionIndex].q;
+  optionsdiv.forEach((e, index) => {
+    e.innerText = data[questionIndex].option[index];
+  });
+}
+
+let interval = setInterval(() => {
+  if (count > 0) {
+    timerdiv.innerText = count;
+  } else {
+    count = 6;
+    questionIndex++;
+    if (questionIndex >= data.length) {
+      questionIndex = 0;
+      quiz.style.display = "none";
+      clearInterval(interval);
+    }
+  }
+  printquestionsandoption();
+  count--;
+}, 1000);
+
+optionsdiv.forEach((e, index) => {
+  e.addEventListener("click", () => {
+    if (e.innerText == data[questionIndex].a) {
+      e.style.backgroundColor = "green";
+    } else {
+      e.style.backgroundColor = "red";
+    }
+  });
+});
